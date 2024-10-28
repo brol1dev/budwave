9caba0c6e2ed7de4b7d651d8785c65c84e556f37
brol1dev <eric.vargas.mx@gmail.com>
2024-10-27 20:41:33 -0500

    Add weights index

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root "weights#index"
+  get "/weights", to: "weights#index", as: :weights
 end
---
+++ test/controllers/weights_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class WeightsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get weights_index_url
+    assert_response :success
+  end
+end
